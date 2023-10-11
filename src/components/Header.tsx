3ef2764169b0dd9a247121b907970d1872eb8e2c
kristenmazza <kbmazza@gmail.com>
2023-10-10 20:49:14 -0700

    Collapse header on scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ import MenuItem from '@mui/material/MenuItem';
 import styles from './Header.module.css';
 import HelpModal from './HelpModal';
 
-const pages = ['Home', 'Leaderboard'];
+const pages = ['New Game', 'Leaderboard'];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
@@ -35,125 +35,147 @@ function ResponsiveAppBar() {
   };
 
   return (
-    <AppBar sx={{ backgroundColor: '#2E3B55' }} position='fixed'>
-      <Container maxWidth='xl'>
-        <Toolbar disableGutters>
-          <Typography
-            variant='h6'
-            noWrap
-            component='a'
-            href='#app-bar-with-responsive-menu'
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            <Box
-              component='img'
-              className={styles.logo}
-              alt='Logo'
-              src='images/logo-speech-sound-sleuth.png'
-            />
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size='large'
-              aria-label='account of current user'
-              aria-controls='menu-appbar'
-              aria-haspopup='true'
-              onClick={handleOpenNavMenu}
-              color='inherit'
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id='menu-appbar'
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign='center'>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant='h5'
-            noWrap
-            component='a'
-            href='#app-bar-with-responsive-menu'
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            <Box
-              component='img'
-              className={styles.logoSmall}
-              alt='Logo'
-              src='images/logo-speech-sound-sleuth.png'
-            />
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+    <>
+      <div style={{ position: 'relative' }}>
+        <AppBar
+          sx={{
+            backgroundColor: '#2E3B55',
+            height: 'auto',
+            position: 'sticky',
+            top: '0',
+            zIndex: 2,
+            boxShadow: 'none',
+          }}
+        >
+          <Container maxWidth='lg'>
+            <Toolbar disableGutters>
+              <Typography
+                noWrap
+                component='a'
+                href='/'
+                sx={{
+                  mr: 2,
+                  display: { xs: 'none', md: 'flex' },
+                }}
               >
-                {page}
-              </Button>
-            ))}
-          </Box>
+                <Box
+                  component='img'
+                  className={styles.logo}
+                  alt='Logo'
+                  src='images/logo-speech-sound-sleuth.png'
+                />
+              </Typography>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <HelpModal />
-            <Menu
-              sx={{ mt: '45px' }}
-              id='menu-appbar'
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            ></Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+              <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                <IconButton
+                  size='large'
+                  aria-label='menu'
+                  aria-controls='menu-appbar'
+                  aria-haspopup='true'
+                  onClick={handleOpenNavMenu}
+                  color='inherit'
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id='menu-appbar'
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: 'block', md: 'none' },
+                  }}
+                >
+                  {pages.map((page) => (
+                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                      <Typography textAlign='center'>{page}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+              <Typography
+                variant='h5'
+                noWrap
+                component='a'
+                href='/'
+                sx={{
+                  mr: 2,
+                  display: { xs: 'flex', md: 'none' },
+                  flexGrow: 1,
+                }}
+              >
+                <Box
+                  component='img'
+                  className={styles.logoSmall}
+                  alt='Logo'
+                  src='images/logo-speech-sound-sleuth.png'
+                />
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                {pages.map((page) => (
+                  <Button
+                    key={page}
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                  >
+                    {page}
+                  </Button>
+                ))}
+              </Box>
+
+              <Box sx={{ flexGrow: 0 }}>
+                <HelpModal />
+                <Menu
+                  sx={{ mt: '45px' }}
+                  id='menu-appbar'
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                ></Menu>
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </div>
+      <AppBar
+        sx={{
+          backgroundColor: 'rgb(28, 131, 128)',
+          height: 'auto',
+          position: 'sticky',
+          top: '0',
+          zIndex: 1,
+          // boxShadow: 'none',
+          // borderBottom: '1px solid #465676',
+        }}
+      >
+        <Container maxWidth='xl'>
+          <Toolbar disableGutters>
+            <div className={styles.gameBar}>
+              <Typography variant='h6'>Timer & Find</Typography>
+            </div>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </>
   );
 }
+
 export default ResponsiveAppBar;
