fabbe203c8e19421c703e402dd45267eed704966
kristenmazza <kbmazza@gmail.com>
2023-10-18 14:00:20 -0700

    Set coordinates of image click

--- src/components/GameImage.tsx
+++ src/components/GameImage.tsx
@@ -3,7 +3,11 @@ import React, { useState } from 'react';
 import ZoomInIcon from '@mui/icons-material/ZoomIn';
 import ZoomOutIcon from '@mui/icons-material/ZoomOut';
 import CenterFocusStrongIcon from '@mui/icons-material/CenterFocusStrong';
-import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+import {
+  TransformWrapper,
+  TransformComponent,
+  ReactZoomPanPinchRef,
+} from 'react-zoom-pan-pinch';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import IconButton from '@mui/material/IconButton';
@@ -18,14 +22,21 @@ export default function GameImage() {
     mouseY: number;
   } | null>(null);
   const [imageLoading, setImageLoading] = useState(true);
+  const [coordinates, setCoordinates] = useState<[number, number]>([0, 0]);
+  const [scale, setScale] = useState<number>(1.01);
 
   const imageLoaded = () => {
     setImageLoading(false);
   };
 
+  const handleImageClick = (event: React.MouseEvent) => {
+    handleImageMenuOpen(event);
+    createTargetCoordinates(event);
+  };
+
   // Toggles image menu's visibility on click by setting its position based on
   // mouse click coordinates
-  const handleImageMenu = (event: React.MouseEvent) => {
+  const handleImageMenuOpen = (event: React.MouseEvent) => {
     event.preventDefault();
     setImageMenu(
       imageMenu === null
@@ -37,12 +48,43 @@ export default function GameImage() {
     );
   };
 
+  const createTargetCoordinates = (event: React.MouseEvent) => {
+    const image = document.querySelector('#gameImage') as HTMLElement;
+
+    if (!image) return;
+
+    const imageRect = image.getBoundingClientRect();
+
+    // Calculate width/height of the zoomed-in image's visible area on the screen
+    const visibleImageWidth = imageRect.width / scale;
+    const visibleImageHeight = imageRect.height / scale;
+
+    // Calculate adjusted position in pixels of click event within zoomed image
+    const xPx = (event.clientX - imageRect.left) / scale;
+    const yPx = (event.clientY - imageRect.top) / scale;
+
+    // Calculate position as a percentage within the visible area of the zoomed image
+    const xPercent = xPx / visibleImageWidth;
+    const yPercent = yPx / visibleImageHeight;
+
+    setCoordinates([xPercent, yPercent]);
+    console.log(xPercent, yPercent);
+  };
+
   const handleImageMenuClose = () => {
     setImageMenu(null);
   };
 
   const toggleControlMenu = () => {
     setIsControlMenuOpen(!isControlMenuOpen);
+  };
+
+  // Set zoom factor (scale)
+  const handleTransform = (
+    _ref: ReactZoomPanPinchRef,
+    state: { scale: number },
+  ) => {
+    setScale(state.scale);
   };
 
   return (
@@ -62,17 +104,19 @@ export default function GameImage() {
           initialScale={1.01}
           initialPositionX={0}
           initialPositionY={0}
+          onTransformed={handleTransform}
         >
           {({ zoomIn, zoomOut, resetTransform }) => (
             <React.Fragment>
               <div className={styles.imageWrapper}>
                 <TransformComponent>
                   <div
-                    onClick={handleImageMenu}
+                    onClick={handleImageClick}
                     style={{ cursor: 'image-menu' }}
                   >
                     <img
                       className={styles.image}
+                      id='gameImage'
                       src='https://kristen-mazza-blog-images.s3.us-west-1.amazonaws.com/uploads/busystreet.png'
                       alt='test'
                       onLoad={imageLoaded}
