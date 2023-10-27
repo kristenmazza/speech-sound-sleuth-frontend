9f2e56aae252482d632ecdf8ef7b06b37a4a6a49
kristenmazza <kbmazza@gmail.com>
2023-10-26 17:12:29 -0700

    Adjust modal styles and content

--- src/components/HelpModal.tsx
+++ src/components/HelpModal.tsx
@@ -11,10 +11,12 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  maxWidth: 400,
+  maxWidth: 600,
   bgcolor: 'rgb(230, 234, 237)',
   boxShadow: 24,
   p: 4,
+  maxHeight: '70%',
+  overflowY: 'auto',
 };
 
 export default function HelpModal() {
@@ -46,7 +48,35 @@ export default function HelpModal() {
             How to Play
           </Typography>
           <Typography id='modal-modal-description' sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            <ol className={styles.modalList}>
+              <li>
+                <b>Pick a Scene:</b> First, choose a fun scene that you like!
+              </li>
+              <li>
+                <b>Select a Sound:</b> Now, select a sound you want to practice.
+                It's like a special mission!
+              </li>
+              <li>
+                <b>Find the Hidden Items:</b> On the game page, click 'View
+                Targets' to see what you need to find.
+              </li>
+              <li>
+                <b>Start the Hunt:</b> Search the scene for the hidden items.
+                When you find one, click it!
+              </li>
+              <li>
+                <b>Practice Time:</b> If you're right, a practice pop-up will
+                appear. You'll be prompted to practice saying the item's name.
+              </li>
+              <li>
+                <b>Keep Going:</b> Keep searching and clicking until you find
+                all the items. The game ends when you've found them all!
+              </li>
+              <li>
+                <b>Quick! The Timer is Running:</b> Be fast, because the timer
+                starts as soon as the game begins.
+              </li>
+            </ol>
           </Typography>
         </Box>
       </Modal>
